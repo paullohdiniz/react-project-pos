8405eb39d1a6cae029d4f04bd76b3e2b41bc93ac
Paulo Diniz <paulo.diniz@emphasysgroup.com.br>
2019-01-06 03:34:45 -0200

    Alteração do projeto com classes reative

---
+++ src/components/TodoItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+
+  getStyle = () => {
+      return {
+          background: 'f4f4f4',
+          padding: '10px',
+          borderBottom: '1px #ccc dotted',
+          textDecoration: this.props.todo.completed ? 
+          'line-through' : 'none'
+      }
+  }
+
+  
+  render() {
+    const { id, title } = this.props.todo; 
+    return (
+      <div style={ this.getStyle() }>
+        <p>
+            <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+            {' '}
+            { title }
+            <button style={btnStyle} onClick= {this.props.delTodo.bind(this, id)}>x</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+TodoItem.PropTypes = {
+    todos: PropTypes.array.isRequired,
+    maskComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+
+const btnStyle = {
+    backgroundColor: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
+export default TodoItem
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>
+          About
+      </h1>
+      <p>Descrição</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
